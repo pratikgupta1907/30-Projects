e4c49229f002386721aeb57837e7cd29d8f48804
pratik gupta <prati1907@gmail.com>
2018-08-01 08:38:25 +0530

    Initial Commit

---
+++ Bulls' Eye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bulls' Eye
+//
+//  Created by Pratik Gupta on 01/08/18.
+//  Copyright © 2018 Pratik Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
