c706bb3c26cb71c7c846db42da10699a10ce430f
pratik gupta <prati1907@gmail.com>
2018-08-01 20:09:48 +0530

    Connect Outlet Fix bug(slider bug)

--- Bulls' Eye/ViewController.swift
+++ Bulls' Eye/ViewController.swift
@@ -9,24 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    var targetValue = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func startNewRound(){
+        targetValue = 1 + Int(arc4random_uniform(100))
+        targetLabel.text = String(targetValue)
+        currentValue = 50
+        slider.value = 50
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        currentValue = lroundf(slider.value)
+    }
 
     @IBAction func showAlert(){
-        let Alert = UIAlertController(title: "HEllo World!", message: "Nothing to display yet", preferredStyle: .alert)
+        let differnce = abs(currentValue - targetValue)
+        let points = 100 - differnce
+        
+        let message = "you scored \(points) points"
+        let Alert = UIAlertController(title: "HEllo World!", message: message , preferredStyle: .alert)
         let action = UIAlertAction(title: "exit", style: .default, handler: nil)
         Alert.addAction(action)
-        
         present(Alert, animated: true, completion: nil)
-        
+        startNewRound()
     }
     
 }
