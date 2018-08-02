fdad9c427b9bbada47d76ff6f1b0834608a61221
pratik gupta <prati1907@gmail.com>
2018-08-02 19:36:29 +0530

    Add AboutMe WebView

--- Bulls' Eye/AboutMeViewController.swift
+++ Bulls' Eye/AboutMeViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class AboutMeViewController: UIViewController {
     
+    @IBOutlet weak var webAboutMeView: UIWebView!
+    
+    
+    
     @IBAction func close (){
         dismiss(animated: true, completion: nil)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "AboutMe", withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseUrl = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webAboutMeView.load(htmlData, mimeType: "text/html", textEncodingName: "utf-8", baseURL: baseUrl)
+            }
+        }
 
-        // Do any additional setup after loading the view.
     }
 
 }
